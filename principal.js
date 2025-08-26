73cd64078b23c11c1a39b8ee93e8b7bac15b80ed
joao pedro <joao.pparella@senacsp.edu.br>
2025-08-26 19:17:45 -0300

    Testando conflitos outro DEV

--- principal.js
+++ principal.js
@@ -18,7 +18,7 @@ var series = [new Serie("Episódio piloto",50,"alguma coisa aconteceu",2020,1,1,
 
 
 const usuario1 = new Usuario('Roberval','robe@email.com','987654321');
-usuario1.senha = '123456';
+usuario1.senha = '123';
 objEntradaSaida.mensagemCompleta(usuario1.senha)
 
 let dados = objEntradaSaida.cadastroCompleto(['nome', 'email', 'senha']);
