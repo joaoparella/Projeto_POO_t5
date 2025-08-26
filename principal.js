d44651f4a65a595001b510c2f2232b0fae5f86ca
joao pedro <joao.pparella@senacsp.edu.br>
2025-08-26 19:58:09 -0300

    Correção de senha

--- principal.js
+++ principal.js
@@ -19,7 +19,7 @@ var series = [new Serie("Episódio piloto",50,"alguma coisa aconteceu",2020,1,1,
 
 const usuario1 = new Usuario('Roberval','robe@email.com','987654321');
 
-usuario1.senha = '000000000000000';
+usuario1.senha = '98123';
 objEntradaSaida.mensagemCompleta(usuario1.senha)
 
 let dados = objEntradaSaida.cadastroCompleto(['nome', 'email', 'senha']);
